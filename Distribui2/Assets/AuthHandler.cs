6ce98bd11c302308c0a8ead5ac5ad8e611215d47
Camilo Correa Rojas <camilo.correar@upb.edu.co>
2023-08-23 01:44:47 -0500

    mevo

--- Distribui2/Assets/AuthHandler.cs
+++ Distribui2/Assets/AuthHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
@@ -8,32 +9,31 @@ using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class AuthHandler : MonoBehaviour
-{ 
+{
     string apiUrl = "https://sid-restapi.onrender.com/api/";
-    
-    
-   [SerializeField] private TMP_InputField usernameInputField;
-   [SerializeField] private TMP_InputField passwordInputField;
-   private string token;
-   private string username;
 
-   private void Start()
-   {
-       token = PlayerPrefs.GetString("token");
-       
-       if (string.IsNullOrEmpty(token))
-       {
-           Debug.Log("No hay Token almacenado");
-       }
-       else
-       {
-           username = PlayerPrefs.GetString("username");
-           StartCoroutine(GetProfile(username));
-       }
-   }
 
-   
-   public void Register()
+    [SerializeField] private TMP_InputField usernameInputField;
+    [SerializeField] private TMP_InputField passwordInputField;
+    private string token;
+    private string username;
+
+    private void Start()
+    {
+        token = PlayerPrefs.GetString("token");
+
+        if (string.IsNullOrEmpty(token))
+        {
+            Debug.Log("No hay Token almacenado");
+        }
+        else
+        {
+            username = PlayerPrefs.GetString("username");
+            StartCoroutine(GetProfile(username));
+        }
+    }
+
+    public void Register()
     {
         AuthData _authData = new AuthData();
         _authData.username = usernameInputField.text;
@@ -43,6 +43,7 @@ public class AuthHandler : MonoBehaviour
 
         StartCoroutine(SendRegister(json));
     }
+
     public void Login()
     {
         AuthData _authData = new AuthData();
@@ -56,13 +57,13 @@ public class AuthHandler : MonoBehaviour
 
     IEnumerator GetProfile(string username)
     {
-        UnityWebRequest www = UnityWebRequest.Get(apiUrl+ "usuarios/" +username);
+        UnityWebRequest www = UnityWebRequest.Get(apiUrl + "usuarios/" + username);
         www.SetRequestHeader("x-token", token);
         yield return www.SendWebRequest();
-        
+
         if (www.isNetworkError)
         {
-            Debug.Log("NETWORK ERROR: "+www.error);
+            Debug.Log("NETWORK ERROR: " + www.error);
         }
         else
         {
@@ -70,54 +71,29 @@ public class AuthHandler : MonoBehaviour
             if (www.responseCode == 200)
             {
                 AuthData data = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
-                Debug.Log("Sesion Activada de usuario "+data.usuario.username);
-               // SceneManager.LoadScene("Game");
+                Debug.Log("Sesion Activada de usuario " + data.usuario.username);
+                // SceneManager.LoadScene("Game");
             }
         }
-      
+
     }
+
     //REGISTER
     IEnumerator SendRegister(string json)
     {
-        UnityWebRequest www = UnityWebRequest.Put(apiUrl+"usuarios", json);
+        UnityWebRequest www = UnityWebRequest.Put(apiUrl + "usuarios", json);
         www.SetRequestHeader("Content-Type", "application/json");
         www.method = "POST";
         yield return www.Send();
 
-        if(www.result==UnityWebRequest.Result.ConnectionError) Debug.Log("Ese pana no existe");
+        if (www.result == UnityWebRequest.Result.ConnectionError) Debug.Log("Ese pana no existe");
         else
         {
             Debug.Log(www.downloadHandler.text);
             if (www.responseCode == 200)
             {
                 AuthData data = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
-                Debug.Log("Usuario registrando con el ID "+data.usuario._id);
-            }
-            else
-            {
-               Debug.Log(www.error);
-            }
-        }
-    }
-    //LOGIN
-    IEnumerator SendLogin(string json)
-    {
-        UnityWebRequest www = UnityWebRequest.Put(apiUrl+"auth/login", json);
-        www.SetRequestHeader("Content-Type", "application/json");
-        www.method = "POST";
-        yield return www.Send();
-
-        if(www.result==UnityWebRequest.Result.ConnectionError) Debug.Log("Ese pana no existe");
-        else
-        {
-            Debug.Log(www.downloadHandler.text);
-            if (www.responseCode == 200)
-            {
-                AuthData data = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
-                
-                Debug.Log("El usuario inicio sesión con éxito con el ID "+data.usuario._id);
-                PlayerPrefs.SetString("token",data.token);
-                PlayerPrefs.SetString("username",data.username);
+                Debug.Log("Usuario registrando con el ID " + data.usuario._id);
             }
             else
             {
@@ -126,19 +102,111 @@ public class AuthHandler : MonoBehaviour
         }
     }
 
+    //LOGIN
+    IEnumerator SendLogin(string json)
+    {
+        UnityWebRequest www = UnityWebRequest.Put(apiUrl + "auth/login", json);
+        www.SetRequestHeader("Content-Type", "application/json");
+        www.method = "POST";
+        yield return www.Send();
+
+        if (www.result == UnityWebRequest.Result.ConnectionError) Debug.Log("Ese pana no existe");
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+            if (www.responseCode == 200)
+            {
+                AuthData data = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
+
+                Debug.Log("El usuario inicio sesión con éxito con el ID " + data.usuario._id);
+                PlayerPrefs.SetString("token", data.token);
+                PlayerPrefs.SetString("username", data.username);
+                Debug.Log("Tu puntaje es  " + data.usuario.data.score);
+            }
+            else
+            {
+                Debug.Log(www.error);
+            }
+        }
+
+        IEnumerator EditScoreData(string json)
+        {
+            UnityWebRequest www = UnityWebRequest.Put(apiUrl + "usuarios/" + username, json);
+            www.SetRequestHeader("x-token", token);
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.method = "PATCH";
+            yield return www.Send();
+
+        }
+
+        IEnumerator ScoreChart(string json)
+        {
+            UnityWebRequest www = UnityWebRequest.Get(apiUrl + "usuarios/");
+            www.SetRequestHeader("x-token", token);
+            www.method = "PATCH";
+            yield return www.Send();
+            if (www.result == UnityWebRequest.Result.ConnectionError) Debug.Log("Ese pana no existe");
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                if (www.responseCode == 200)
+                {
+                    UserList jsonData = JsonUtility.FromJson<UserList>(www.downloadHandler.text);
+
+                    List<User> userList = jsonData.usuarios;
+                    List<User> usuariosOrdenados = userList.OrderByDescending(x => x.data.score).ToList<User>();
+
+                    int index = 0;
+
+                    foreach (User user in usuariosOrdenados)
+                    {
+                        if (index <= 4)
+                        {
+                            string userScore = index + 1 + " - " + user.username + " Score: " + user.data.score;
+                        }
+                    }
+
+                }
+                else
+                {
+                    Debug.Log(www.error);
+                }
+            }
+        }
+
+    }
 }
+
 [System.Serializable]
 public class AuthData
 {
     public string username;
     public string password;
-    public UserData usuario;
+    
+    public User usuario;
     public string token;
+
+    //public User[] usuarios;
 }
 [System.Serializable]
-public class UserData
+public class User
 {
     public string _id;
     public string username;
     public bool estado;
+    public Data data;
 }
+
+[System.Serializable]
+public class Data
+{
+    public int score;
+    //public User[] friends;
+}
+
+[System.Serializable]
+public class UserList
+{
+    public List<User> usuarios;
+}  
+

170fdbc181fdf7a6a367cec3eb71c8ee886cc53e
Camilo Correa Rojas <camilo.correar@upb.edu.co>
2023-08-22 22:53:03 -0500

    Dolor

--- Distribui2/Assets/AuthHandler.cs
+++ Distribui2/Assets/AuthHandler.cs
@@ -7,59 +7,58 @@ using UnityEngine.Networking;
 
 public class AuthHandler : MonoBehaviour
 {
-
     public string apiUrl = "https://sid-restapi.onrender.com/api";
+    
     
    [SerializeField] private TMP_InputField usernameInputField;
    [SerializeField] private TMP_InputField passwordInputField;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+   
     public void Register()
     {
-        string username = usernameInputField.text;
-        string password = passwordInputField.text;
+        AuthData _authData = new AuthData();
+        _authData.username = usernameInputField.text;
+        _authData.password = passwordInputField.text;
+
+        string json = JsonUtility.ToJson(_authData);
+
+        StartCoroutine(SendRegister(json));
     }
 
     IEnumerator SendRegister(string json)
     {
         UnityWebRequest www = UnityWebRequest.Put(apiUrl+"/usuarios", json);
         www.SetRequestHeader("Content-Type", "application/jason");
-        
+        www.method = "POST";
         yield return www.Send();
 
-        if(www.result==UnityWebRequest.Result.ConnectionError) Debug.Log("Ese maestro no existe");
+        if(www.result==UnityWebRequest.Result.ConnectionError) Debug.Log("Ese pana no existe");
         else
-        {/*
+        {
+            Debug.Log(www.downloadHandler.text);
             if (www.responseCode == 200)
             {
-                Trainer trainer = JsonUtility.FromJson<Trainer>(www.downloadHandler.text);
-                
-                StartCoroutine(DownloadImage(trainer.img, trainerPhoto));
-                trainerNameTMP.text = trainer.name;
-                
-                for (int i = 0; i < trainer.deck.Length; i++)
-                {
-                    pokemonTeam[i].SearchPokemon(trainer.deck[i]);
-                    yield return new WaitForSeconds(0.1f);
-                }
+                AuthData data = JsonUtility.FromJson<AuthData>(www.downloadHandler.text);
+                Debug.Log("Usuario registrando con el ID "+data.usuario._id);
             }
             else
             {
-                string error_message = "Status: " + www.responseCode;
-                error_message += "\nContent-Type: " + www.GetResponseHeader("content-type") + "\nError:" + www.error;
-                Debug.Log(error_message);
-            }*/
+               Debug.Log(www.error);
+            }
         }
     }
 
 }
-
+[System.Serializable]
+public class AuthData
+{
+    public string username;
+    public string password;
+    public UserData usuario;
+}
+[System.Serializable]
+public class UserData
+{
+    public string _id;
+    public string username;
+    public bool estado;
+}
